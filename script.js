352b8d0bb374e0f1d59e003c17183b9c97b19264
William Fernandes <wiilfern1910@outlook.com>
2023-07-03 20:27:12 -0300

    Part II

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ menuBtn.addEventListener("click", (ev) => {
   navBar.classList.toggle("appear");
 });
 
-var counter = document.getElementById('experienceYears');
+let counter = document.getElementById("experienceYears");
 
-function increment(i, max){
-  if(i > max) return;
-  setTimeout(function(){
-    counter.innerText = i ;
-    increment(i+1, max);
-  },80)
+function increment(i, max) {
+  if (i > max) return;
+  setTimeout(function () {
+    counter.innerText = i;
+    increment(i + 1, max);
+  }, 80);
 }
 
-increment(0,20);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      if (entry.target.dataset.box == "experience-box") {
+        increment(0, 20);
+      }
+    }
+    entry.target.unobserve();
+  });
+});
+
+const hiddenContent = document.querySelectorAll(".hidden");
+hiddenContent.forEach((ev) => observer.observe(ev));
